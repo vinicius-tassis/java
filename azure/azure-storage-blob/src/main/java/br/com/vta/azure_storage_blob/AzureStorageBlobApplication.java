d019d11e78efbc0891f361b04b0a1c8ced44d6b9
Vinicius <vtassis@gmail.com>
2025-02-23 19:44:37 -0300

    storage blob azure

---
+++ azure/azure-storage-blob/src/main/java/br/com/vta/azure_storage_blob/AzureStorageBlobApplication.java
@@ -0,0 +1,15 @@
+package br.com.vta.azure_storage_blob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureStorageBlobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureStorageBlobApplication.class, args);
+
+		
+	}
+
+}
---
+++ azure/azure-storage-blob/src/test/java/br/com/vta/azure_storage_blob/AzureStorageBlobApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vta.azure_storage_blob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureStorageBlobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
